b75caf1e8b153dc4d87d390adb0694c4d1b89bf8
mariajiwuaku <mariagorettiduruigbo@gmail.com>
2023-08-31 19:15:48 +0100

    added dismiss button

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const emailInput = document.querySelector(".email-input");
 
 const emailSuccessCard = document.querySelector(".email-success-card");
 const emailSuccessInfo = document.querySelector(".email-success-info");
+const dismissButton = document.querySelector(".dismiss-button");  // Add this line
 
 document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault(e);
+  e.preventDefault();
   let emailChecker = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  if (!emailChecker.test(emailInput.value) || emailInput.value === null) {
+  if (!emailChecker.test(emailInput.value) || emailInput.value === "") {
     emailInput.classList.add("invalid");
     emailAdvice.classList.add("invalid");
   } else {
     emailCard.classList.add("inactive");
+    emailSuccessCard.classList.remove("inactive");  // Remove "inactive" class to show the success card
     emailSuccessCard.classList.add("active");
     emailSuccessInfo.textContent = emailInput.value;
   }
+});
+
+dismissButton.addEventListener("click", () => {
+  emailCard.classList.remove("inactive");  // Show the form again
+  emailSuccessCard.classList.remove("active");  // Hide the success card
+  emailSuccessCard.classList.add("inactive");
+  emailInput.value = "";  // Clear the email input
+  emailInput.classList.remove("invalid");
+  emailAdvice.classList.remove("invalid");
 });
